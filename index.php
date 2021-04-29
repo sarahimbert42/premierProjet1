3d3ba4f57fe41b28f41d762477df29251b2c8a8f
Bibounette75 <sarah.imbert@hotmail.fr>
2021-04-29 15:43:24 +0200

    premierProjet1

--- index.php
+++ index.php
@@ -9,4 +9,13 @@ const DB_PASSWORD   = 'root';
 $pdo = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
 $pdo->exec('SET NAMES UTF8');
 
+
+$stmt = $pdo->prepare('SELECT productName, productDescription, buyPrice
+    FROM products ');
+    $stmt->execute();
+    $order = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $json = json_encode($order);
+    echo $json
+
+
 ?>
